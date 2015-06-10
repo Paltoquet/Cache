98b93d95bd8a296222f736400b77eb608a56658d
Seb <s.nico@live.fr>
2015-06-10 11:22:50 +0200

    fonction Cache_Create dans cache.c

--- Cache_Project/cache.c
+++ Cache_Project/cache.c
@@ -1,10 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "cache.h"
+#include "low_cache.h"
 
 
 //! Création du cache.
 struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
                            size_t recordsz, unsigned nderef){
 
+    struct Cache* cache = (struct Cache*) malloc(sizeof(struct Cache));
+    cache->file = fic;
+    cache->fp = fopen(fic, "r" );
+    cache->nblocks = nblocks;
+    cache->nrecords = nrecords;
+    cache->recordsz = recordsz;
+    cache->blocksz = recordsz * nrecords;
+    cache->nderef = nderef;
+    cache->pstrategy = NULL;
+
+    //init instrument
+    struct Cache_Instrument* cacheInstrument = (Cache_Instrument*) malloc(sizeof(struct Cache_Instrument));
+    cacheInstrument->n_reads=0;
+    cacheInstrument->n_writes=0;
+    cacheInstrument->n_hits=0;
+    cacheInstrument->n_syncs=0;
+    cacheInstrument->n_deref=0;
+
+    //init Headers
+    struct Cache_Block_Header* headers = (struct Cache_Block_Header*) malloc(sizeof(struct Cache_Block_Header)*nblocks);
+    cache->pfree = headers;
+    cache->headers = headers;
 }
 
 //! Fermeture (destruction) du cache.
