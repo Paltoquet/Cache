d7b49f2a1829b8019d1234d45a933d808652d078
jeanloicmugnier <mugnier@polytech.unice.fr>
2015-06-10 15:56:13 +0200

    methods : cache write and cache get instrument

--- Cache_Project/cache.c
+++ Cache_Project/cache.c
@@ -3,6 +3,8 @@
 #include "cache.h"
 #include "low_cache.h"
 #include "strategy.h"
+#include <string.h>
+
 
 
 //! Création du cache.
@@ -34,6 +36,7 @@ struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
 
     //strategie
     cache->pstrategy = Strategy_Create(cache);
+
 }
 
 //! Fermeture (destruction) du cache.
@@ -57,25 +60,62 @@ Cache_Error Cache_Close(struct Cache *pcache){
 
 //! Synchronisation du cache.
 Cache_Error Cache_Sync(struct Cache *pcache){
-
+    return CACHE_OK;
 }
 
 //! Invalidation du cache.
 Cache_Error Cache_Invalidate(struct Cache *pcache){
-
+    return CACHE_OK;
 }
 
 //! Lecture  (à travers le cache).
-Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord){
-
+Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord)
+{
+    return CACHE_OK;
 }
 
 //! Écriture (à travers le cache).
 Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord)
 {
     // recherche du bloc irfile dans le cache
+    struct Cache_Block_Header *tmp = pcache->headers;
+    struct Cache_Block_Header *next = tmp;
+    int block_courant = 0;
+    int is_in_cache = 0;
 
-    // ecrit le contenu du buffer precords dans le block
+    while ( !is_in_cache && block_courant++ < pcache->nblocks ) {
+        next++;
+        if(next->ibfile == irfile)
+        {
+            // ecrit le contenu du buffer precords dans le block
+            strcpy(next->data,precord);
+            //Modification des flags
+            if(next->flags & 0) // le bloc est invalide
+                next->flags = VALID ;
+
+            if(next->flags & VALID) // le bloc possede des donnees et on ecrit dessus
+                next->flags = MODIF ;
+            is_in_cache = 1 ;
+
+
+        }
+    }
+
+    if(!is_in_cache)
+    {
+        next = Strategy_Replace_Block(pcache);
+        strcpy(next->data,precord);
+        //Modification des flags
+        if(next->flags & 0) // le bloc est invalide
+            next->flags = VALID ;
+
+        if(next->flags & VALID) // le bloc possede des donnees et on ecrit dessus
+            next->flags = MODIF ;
+    }
+
+    //appel a write de la strategie utilise
+    Strategy_Write(pcache,next);
+    return CACHE_OK;
 }
 
 
@@ -87,7 +127,7 @@ struct Cache_Instrument *Cache_Get_Instrument(struct Cache *pcache)
             Attention : tous les compteurs de la structure courante sont remis à 0 par cette
     fonction.*/
 
-    struct Cache_Instrument * inst = pcache->instrument;
+    struct Cache_Instrument * inst =  &pcache->instrument;
     pcache->instrument.n_deref = 0;
     pcache->instrument.n_hits = 0;
     pcache->instrument.n_reads = 0;
