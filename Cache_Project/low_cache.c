1823231ea5e5cda36798d559bdb1ee728ce4ca51
Seb <s.nico@live.fr>
2015-06-10 10:33:38 +0200

    Low_cache -> fontion Get_Free_Block

---
+++ Cache_Project/low_cache.c
@@ -0,0 +1,12 @@
+struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache){
+
+    if( pcache->pfree != NULL ) return pcache->pfree;
+
+    for(int i = 0; i < pcache->nblocks; i++){
+
+        if( pcache->headers[i].flags & VALID ){
+            return pcache->headers+i;
+        }
+        return NULL;
+    }
+}
